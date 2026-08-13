0e9bf6a5a7807eb01eb5e8c45b2b04328f3dc110
johnsimao18-eng <johnsimao18@gmail.com>
2026-08-13 14:00:03 +0200

    got the servuce account key

--- index.js
+++ index.js
@@ -204,26 +204,26 @@ app.put("/events/:id", async (req, res) => {
 });
 
 // endpoint to delete events
-router.delete("/:id", verifyFirebase, async (req, res) => {
-    try {
-        const { id } = req.params;
-        const collection = db.collection("events");
-        const result = await collection.deleteOne({_id: new ObjectId(id)});
-        if (result.deletedCount === 0) {
-          return res.status(404).json({
-            message: "Event not found"
-          });
-        }
-        res.status(200).json({
-          message: "Event deleted successfully"
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({
-          message: error.message
-        });
-    }
-});
+// router.delete("/:id", verifyFirebase, async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         const collection = db.collection("events");
+//         const result = await collection.deleteOne({_id: new ObjectId(id)});
+//         if (result.deletedCount === 0) {
+//           return res.status(404).json({
+//             message: "Event not found"
+//           });
+//         }
+//         res.status(200).json({
+//           message: "Event deleted successfully"
+//         });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(400).json({
+//           message: error.message
+//         });
+//     }
+// });
 
 // endpoint to get venues
 app.get("/venues", verifyFirebase, async (req, res) => {
