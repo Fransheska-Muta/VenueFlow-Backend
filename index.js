bad4a60330b7d3267edc06a49bd046ff1f9d1256
mutafransheska45-maker <mutafransheska45@gmail.com>
2026-08-19 13:27:05 +0200

    fixed the login issue

--- index.js
+++ index.js
@@ -76,21 +76,20 @@ async function verifyFirebase(req, res, next) {
 }
 }
 
+// handles login
 app.get("/users/:uid", async (req, res) => {
-  const collection = db.collection("users");
-  const user = await collection.findOne({
-    uid: req.params.uid,
-  });
-  if (!user) {
-    return res.status(404).json({
-      message: "User not found",
-    });
+  try {
+    const collection = db.collection("users");
+    const user = await collection.findOne({uid: req.params.uid});
     if (!user) {
-        return res.status(404).json({message: "User not found"});
+      return res.status(404).json({message: "User not found",});
     }
     res.json(user);
-}});
-
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).json({message: "Internal Server Error",});
+  }
+});
 
 //this gets all users who have roles of manager only and puts them in the table on the superadmin dashboard
 app.get("/users", verifyFirebase, async (req, res) => {
